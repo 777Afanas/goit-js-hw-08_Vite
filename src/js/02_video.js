da5fcf4e7edaf6cc4a5282e14250580156ef767c
Serhii Yatsenko <serhii2111@yahoo.com>
2024-02-03 18:35:51 +0200

    removed-02_next-step

--- src/js/02_video.js
+++ src/js/02_video.js
@@ -1,18 +1,18 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-// const onPlay = function ({seconds}) {
-//     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+// // const onPlay = function ({seconds}) {
+// //     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+// // }
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// function onPlay(data) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds); // JSON.stringify(data.seconds)
 // }
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay(data) {
-  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds); // JSON.stringify(data.seconds)
-}
-
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
